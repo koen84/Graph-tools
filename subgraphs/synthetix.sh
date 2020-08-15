f6812ad43824fbd44518598a716c74a37129c032
git rpi-backup <git@rpi-backup>
2020-08-15 02:03:32 +0200

    subgraph synthetix

---
+++ subgraphs/synthetix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installer for 'Graph-subgraph' on 'Ubuntu 18.04'"
+echo "Using httpie instead of graph-cli"
+
+# sudo apt-get install httpie
+
+http post 127.0.0.1:8020 jsonrpc="2.0" method="subgraph_create" id="3" params:='{"name": "synthetixio-team/synthetix"}'
+http post 127.0.0.1:8020 jsonrpc="2.0" id="3" method="subgraph_deploy" params:='{"name": "synthetixio-team/synthetix", "ipfs_hash": "QmSXzWZhbDgDg9YyDeivFqYBc1zxESyBYBH3W9CzmupYQd", "node:id":"graph_node1"}'
+
+# for redeploying to another server (or stop indexing if unexisting node-id)
+#http post 127.0.0.1:8020 jsonrpc="2.0" id="1" method="subgraph_reassign" params:='{"name": "synthetixio-team/synthetix", "ipfs_hash": "QmSXzWZhbDgDg9YyDeivFqYBc1zxESyBYBH3W9CzmupYQd", "node_id": "nothing"}'
