0e856900e6a6c9dd42d63ecfad7750aff879d501
git rpi-backup <git@rpi-backup>
2020-08-18 15:59:51 +0200

    subgraphs - fix node_id typo

--- subgraphs/gravity.sh
+++ subgraphs/gravity.sh
@@ -6,7 +6,7 @@ echo "Using httpie instead of graph-cli"
 # sudo apt-get install httpie
 
 http post 127.0.0.1:8020 jsonrpc="2.0" method="subgraph_create" id="4" params:='{"name": "jannis/gravity"}'
-http post 127.0.0.1:8020 jsonrpc="2.0" id="4" method="subgraph_deploy" params:='{"name": "jannis/gravity", "ipfs_hash": "QmbeDC4G8iPAUJ6tRBu99vwyYkaSiFwtXWKwwYkoNphV4X", "node:id":"graph_node1"}'
+http post 127.0.0.1:8020 jsonrpc="2.0" id="4" method="subgraph_deploy" params:='{"name": "jannis/gravity", "ipfs_hash": "QmbeDC4G8iPAUJ6tRBu99vwyYkaSiFwtXWKwwYkoNphV4X", "node_id":"graph_node1"}'
 
 # for redeploying to another server (or stop indexing if unexisting node-id)
 #http post 127.0.0.1:8020 jsonrpc="2.0" id="4" method="subgraph_reassign" params:='{"name": "jannis/gravity", "ipfs_hash": "QmXKwSEMirgWVn41nRzkT3hpUBw29cp619Gx58XW6mPhZP", "node_id": "nothing"}'
--- subgraphs/moloch.sh
+++ subgraphs/moloch.sh
@@ -6,8 +6,7 @@ echo "Using httpie instead of graph-cli"
 # sudo apt-get install httpie
 
 http post 127.0.0.1:8020 jsonrpc="2.0" method="subgraph_create" id="1" params:='{"name": "molochventures/moloch"}'
-http post 127.0.0.1:8020 jsonrpc="2.0" id="1" method="subgraph_deploy" params:='{"name": "molochventures/moloch", "ipfs_hash": "QmTXzATwNfgGVukV1fX2T6xw9f6LAYRVWpsdXyRWzUR2H9", "node:id":"graph_node1"}'
+http post 127.0.0.1:8020 jsonrpc="2.0" id="1" method="subgraph_deploy" params:='{"name": "molochventures/moloch", "ipfs_hash": "QmTXzATwNfgGVukV1fX2T6xw9f6LAYRVWpsdXyRWzUR2H9", "node_id":"graph_node1"}'
 
 # for redeploying to another server (or stop indexing if unexisting node-id)
 #http post 127.0.0.1:8020 jsonrpc="2.0" id="1" method="subgraph_reassign" params:='{"name": "molochventures/moloch", "ipfs_hash": "QmTXzATwNfgGVukV1fX2T6xw9f6LAYRVWpsdXyRWzUR2H9", "node_id": "nothing"}'
-
