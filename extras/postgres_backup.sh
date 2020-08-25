2e9657e259e3b973c93b14b715cc7e9ee0e1f9b3
git rpi-backup <git@rpi-backup>
2020-08-26 00:55:23 +0200

    pg backup - user as var

--- extras/postgres_backup.sh
+++ extras/postgres_backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # vars
 dest=$HOME/backups
+pguser=postgres
 today=$(date +%y%m%d-%H%M)
 
 #logic
 mkdir -p $dest
-sudo -u postgres pg_dumpall | gzip -cn > $dest/pg_backup_$today.gz
+sudo -u $pguser pg_dumpall | gzip -cn > $dest/pg_backup_$today.gz
