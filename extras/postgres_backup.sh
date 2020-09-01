b3caecb5e6eeac9bf8a48eeca8a56690310331a0
git rpi-backup <git@rpi-backup>
2020-09-01 05:40:31 +0200

    extra postgres backup - pigz compression (3x faster)

--- extras/postgres_backup.sh
+++ extras/postgres_backup.sh
@@ -4,6 +4,10 @@ dest=$HOME/backups
 pguser=postgres
 today=$(date +%y%m%d-%H%M)
 
+echo "pigz results same data size, but is about 3 times faster than gzip"
+echo "install pigz with 'sudo apt-get install pigz' and then (un)comment the last 2 lines"
+
 #logic
 mkdir -p $dest
 sudo -u $pguser pg_dumpall | gzip -cn > $dest/pg_backup_$today.gz
+#sudo -u postgres pg_dumpall | pigz > $dest/pg_backup_$today.gz
